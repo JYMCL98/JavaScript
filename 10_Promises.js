ee77dece9d24d1bd4b2b20fad32f4cdc53df588a
Jym Emmanuel Cocotle Lara <jymcl98@gmail.com>
2023-02-21 14:11:03 -0600

    0.1

--- 10_Promises.js
+++ 10_Promises.js
@@ -28,3 +28,46 @@ getData(false)
     });
 
 console.log('Fin');
+
+// async - await
+const getData2 = (error) => new Promise((resolve, reject) => {
+    if (!error) {
+        setTimeout(() => {
+            resolve({
+                nombre: 'Jym',
+                apellido: 'Lara',
+            });
+        }, 3000)
+        
+    }else{
+        reject('No pudimos obtener los datos')
+    }
+});
+
+const getData3 = (error) => new Promise((resolve, reject) => {
+    if (!error) {
+        setTimeout(() => {
+            resolve({
+                nombre: 'Luis',
+                apellido: 'Sanchez',
+            });
+        }, 3000)
+        
+    }else{
+        reject('No pudimos obtener los datos de data 3')
+    }
+});
+
+const main = async () => {
+    try {
+        let resultado = await getData2(false)
+    let resultado2 = await getData3(true)
+    console.log(resultado);
+    console.log(resultado2);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
+
